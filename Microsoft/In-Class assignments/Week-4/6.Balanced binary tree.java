d10a81b0b4eccdcdb7c20b9a4d5c0bb46a5f3b87
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-09-18 21:40:13 +0530

    Update 6.Balanced binary tree.java

--- Microsoft/In-Class assignments/Week-4/6.Balanced binary tree.java
+++ Microsoft/In-Class assignments/Week-4/6.Balanced binary tree.java
@@ -1,1 +1,20 @@
+class Solution {
+  public boolean isBalanced(TreeNode root) {
+    if (helper(root) == -1) return false;
 
+    return true;
+  }
+  int helper(TreeNode root) {
+
+    if (root == null) {
+      return 0;
+
+    }
+
+    int l = helper(root.left);
+    int r = helper(root.right);
+    if (l == -1 || r == -1 || Math.abs(l - r) > 1) return -1;
+    else return Math.max(l, r) + 1;
+
+  }
+}
