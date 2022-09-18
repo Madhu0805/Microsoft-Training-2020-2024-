c8f54aab5aa6b8d07f37a28443a012cbf4123527
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-09-18 21:41:02 +0530

    Update 7.Validate binary search tree.java

--- Microsoft/In-Class assignments/Week-4/7.Validate binary search tree.java
+++ Microsoft/In-Class assignments/Week-4/7.Validate binary search tree.java
@@ -1,1 +1,25 @@
 
+class Solution {
+
+Integer prev = null;
+public boolean isValidBST(TreeNode root) {
+    if(root == null){
+        return true;
+    }
+     
+    boolean left = isValidBST(root.left);
+    
+    if(prev == null){
+        prev = root.val;
+    }else if(root.val <= prev){
+        return false;
+    }else{
+        prev = root.val;
+    }
+    
+    boolean right =  isValidBST(root.right);
+    
+    return left && right;
+}
+}
+
