0f4d13cc25f7f301e70350b2ea4703e93582c018
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-09-18 21:36:32 +0530

    Update 2.Binary tree inorder traversal.java

--- Microsoft/In-Class assignments/Week-4/2.Binary tree inorder traversal.java
+++ Microsoft/In-Class assignments/Week-4/2.Binary tree inorder traversal.java
@@ -1,1 +1,15 @@
 
+class Solution {
+    List<Integer>arr=new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        preorder(root);
+        return arr;
+    }
+    public void preorder(TreeNode root){
+        if(root!=null){
+            preorder(root.left);
+            arr.add(root.val);
+            preorder(root.right);
+        }
+    }
+}
