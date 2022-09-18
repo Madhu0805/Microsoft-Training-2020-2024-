7f6d4caeb8055a42f0b14f4fac37f93ffd0f2535
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-09-18 20:46:54 +0530

    Update 1.Valid Paranthesis.java

--- Microsoft/In-Class assignments/Week-3/1.Valid Paranthesis.java
+++ Microsoft/In-Class assignments/Week-3/1.Valid Paranthesis.java
@@ -1,1 +1,22 @@
 
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()) {
+            if(c == '(' ||c == '{' || c == '[')
+                stack.push(c);
+            else {
+                if(stack.isEmpty())
+                    return false;
+                else {
+                    char top = stack.peek();
+                    if(top == '(' && c != ')' || top == '{' && c != '}' || top == '[' && c != ']')
+                        return false;
+                    
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
