6b489b0c9e7c1bc14fe52913278737c08421d6ea
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-09-18 21:37:28 +0530

    Update 3.Delete node in a BST.java

--- Microsoft/In-Class assignments/Week-4/3.Delete node in a BST.java
+++ Microsoft/In-Class assignments/Week-4/3.Delete node in a BST.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        
+        if (root.val == key) {
+            // delete
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            
+            TreeNode minNode = getMin(root.right);
+            root.right = deleteNode(root.right, minNode.val);
+            minNode.left = root.left;
+            minNode.right = root.right;
+            root = minNode;
+        }
+        
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        }
+        
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+    
+    public TreeNode getMin(TreeNode node) {
+        while (node.left != null) node = node.left;
+        return node;
+    }
+}
