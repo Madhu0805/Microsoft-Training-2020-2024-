7d8a98406136270c796c6a943a0c3fbdc5d75a02
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-09-18 21:38:27 +0530

    Update 4.Binary tree postorder traversal.java

--- Microsoft/In-Class assignments/Week-4/4.Binary tree postorder traversal.java
+++ Microsoft/In-Class assignments/Week-4/4.Binary tree postorder traversal.java
@@ -1,1 +1,26 @@
 
+class Solution {
+    public List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null){
+            return list;
+        }
+        if(root.left != null){
+            callPostorder(root.left);
+        }
+        if(root.right != null){
+            callPostorder(root.right);
+        }
+        list.add(root.val);
+        return list;
+    }
+    public void callPostorder(TreeNode node){
+        if(node.left != null){
+            callPostorder(node.left);
+        }
+        if(node.right != null){
+            callPostorder(node.right);
+        }
+        list.add(node.val);
+    }
+}
