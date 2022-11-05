e2baf93e10cad4281813c6ac0897e3b3bfb10374
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-11-05 15:51:50 +0530

    Update Search a 2D matrix II.java

--- Microsoft/Question Bank/Google/Search a 2D matrix II.java
+++ Microsoft/Question Bank/Google/Search a 2D matrix II.java
@@ -1,11 +1,14 @@
 class Solution {
-public boolean searchMatrix(int[][] matrix, int target) {
-for(int i=0;i< matrix.length;i++){
-for(int j=0;j< matrix[0].length;j++){
-if(matrix[i][j] == target)
-return true;
-}
-}
-return false;
-}
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==target)
+                    return true;
+            }
+        }
+        return false;
+    }
 }
