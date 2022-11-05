2017f8e788c05167a653447d1eb956ecbe59ed01
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-11-05 15:37:06 +0530

    Update Merge k Sorted Lists.java

--- Microsoft/Question Bank/Google/Merge k Sorted Lists.java
+++ Microsoft/Question Bank/Google/Merge k Sorted Lists.java
@@ -1,1 +1,22 @@
 
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+         ListNode head=new ListNode(0);
+       ListNode temp=head;
+       List<Integer> ls=new ArrayList<>();
+       
+       for(ListNode node: lists){
+         while(node!=null){
+           ls.add(node.val);
+             node=node.next;
+         }         
+       }
+       Collections.sort(ls);
+       for(int val: ls){
+         temp.next=new ListNode(val);
+         temp=temp.next;
+       }
+       return head.next;
+    }
+}
