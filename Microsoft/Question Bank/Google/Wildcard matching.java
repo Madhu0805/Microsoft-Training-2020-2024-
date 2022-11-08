50f86e450f946dd2b52402d00f1a47c3257ca9ec
Madhu0805 <108139204+Madhu0805@users.noreply.github.com>
2022-11-08 11:53:35 +0530

    Update Wildcard matching.java

--- Microsoft/Question Bank/Google/Wildcard matching.java
+++ Microsoft/Question Bank/Google/Wildcard matching.java
@@ -1,1 +1,18 @@
 
+class Solution{
+    public boolean isMatch(String s, String p) {
+	boolean match[][] = new boolean[s.length() + 1][p.length() + 1];
+	match[0][0] = true;
+	for (int i = 0; i < p.length(); i++) // judge whether the substring of p matchese empty sequence
+		if (p.charAt(i) == '*')
+			match[0][i + 1] = match[0][i];
+	for (int i = 0; i < s.length(); i++)
+		for (int j = 0; j < p.length(); j++) {
+			if( p.charAt(j) == '*' )
+				match[i + 1][j + 1] = (match[i][j + 1] | match[i + 1][j] | match[i][j]);
+			else if( s.charAt(i) == p.charAt(j) || p.charAt(j) == '?' )
+				match[i + 1][j + 1] = match[i][j];
+		}
+	return match[s.length()][p.length()];
+}
+}
